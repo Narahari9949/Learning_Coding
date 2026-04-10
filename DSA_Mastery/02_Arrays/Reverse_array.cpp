3fac6cdabfa9304c5e0f0533ac662b80e5d8b418
Narahari9949 <141667446+Narahari9949@users.noreply.github.com>
2026-04-10 23:29:27 +0530

    Reverse array

---
+++ DSA_Mastery/02_Arrays/Reverse_array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void Reversearray(int arr[] , int n){
+    int start = 0;
+    int end = n-1;
+
+    while(start < end){
+        swap(arr[start] , arr[end]);
+        start++;
+        end--;
+    }
+
+}
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    Reversearray(arr , 5);
+
+    cout<<"Reversed array is : ";
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
